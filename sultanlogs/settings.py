84c8ea0fb9fe3784f867bf6b7a7f846efacd25bd
sultan <sultan.panar@gmail.com>
2020-10-22 03:18:05 +0600

    added bootstrap

--- sultanlogs/settings.py
+++ sultanlogs/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'crispy_forms',
 ]
+
+CRISPY_template_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
